e05fd0066e42e74e8a41def9c4b3a0357f0ad0d7
Philip Castiglione <philipcastiglione@gmail.com>
2024-12-17 14:47:16 +0800

    add second user in prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@ unless User.exists?(email_address: email_address)
   user.password_confirmation = password
   user.save!
 end
+
+# in prod add additional user
+if Rails.env.production?
+  email_address = Rails.application.credentials.dig(:user_two, :email)
+  password = Rails.application.credentials.dig(:user_two, :password)
+
+  unless User.exists?(email_address: email_address)
+    user_two = User.new(email_address: email_address)
+    user_two.password = password
+    user_two.password_confirmation = password
+    user_two.save!
+  end
+end
